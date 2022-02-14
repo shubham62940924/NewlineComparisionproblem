a349277d4e5a7e965b6a57d972b3d50fad3267ea
first_name <ss329222@gmail.com>
2022-02-14 10:20:23 +0530

    Added:welcome message

---
+++ src/com/blz/demo/LineComparisionProblem.java
@@ -0,0 +1,11 @@
+package com.blz.demo;
+
+public class LineComparisionProblem {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line computation problem");
+
+	}
+	// TODO Auto-generated method stub
+
+}
